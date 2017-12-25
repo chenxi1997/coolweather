e76e4ed9a7ac813bceb31ccfc1f317dba5c29589
chenxi1997 <573499920@qq.com>
2017-12-26 04:17:34 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/AQI.java
@@ -0,0 +1,18 @@
+package android.coolweather.com.coolweather.gson;
+
+/**
+ * Created by Think on 2017/12/26.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/Now.java
@@ -0,0 +1,21 @@
+package android.coolweather.com.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Think on 2017/12/26.
+ */
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+
+        @SerializedName("txt")
+        public String info;
+    }
+}
